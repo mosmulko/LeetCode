ef97f2e501a3db735c90da2b9f7c5aef1e56b9b0
Monika Smulko <monika.smulko@gmail.com>
2021-06-01 12:22:21 +0200

    WIP - Update nr 20 algorithm that fails last test

--- easy/strings/20.ValidParentheses.js
+++ easy/strings/20.ValidParentheses.js
@@ -1,11 +1,26 @@
 function isValid(string) {
+  if (string.length % 2 === 1) return false;
   const parenthesesPairs = {
     "(": ")",
     "[": "]",
     "{": "}",
   };
-  if (parenthesesPairs[string[0]] === string[1]) return true;
-  return false;
+  let lastIndex = string.length - 1;
+  let skippedPair = 0;
+  for (let i = 0; i <= lastIndex; i++) {
+    if (parenthesesPairs[string[i]] === string[i + 1]) {
+      i++;
+      skippedPair += 2;
+    } else if (
+      parenthesesPairs[string[i]] ===
+      string[string.length - 1 - i + skippedPair]
+    ) {
+      lastIndex--;
+    } else {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isValid;
--- easy/strings/20.ValidParentheses.test.js
+++ easy/strings/20.ValidParentheses.test.js
@@ -5,3 +5,37 @@ test("checks if parentheses is closed with only 2 characters of same pair", () =
   expect(isValid("[]")).toBeTruthy();
   expect(isValid("{}")).toBeTruthy();
 });
+
+test("checks if parentheses is closed with consecutive pairs", () => {
+  expect(isValid("()[]")).toBeTruthy();
+  expect(isValid("[]{}()")).toBeTruthy();
+  expect(isValid("{}[)")).toBeFalsy();
+});
+
+test("checks if parentheses are valid when they are scoped within each other", () => {
+  expect(isValid("([])")).toBeTruthy();
+  expect(isValid("[{()}]")).toBeTruthy();
+  expect(isValid("{[)}")).toBeFalsy();
+});
+
+test("checks if parentheses are valid when they are both scoped and consecutive", () => {
+  expect(isValid("([(){}])")).toBeTruthy();
+});
+
+test("checks if function return false when there's an odd number of characters", () => {
+  expect(isValid("[])")).toBeFalsy();
+});
+
+test("checks if function return false when brackets are inverted", () => {
+  expect(isValid("][)(")).toBeFalsy();
+  expect(isValid("]}{[")).toBeFalsy();
+});
+
+test("checks if parentheses are valid when they are both scoped and consecutive assymetrically", () => {
+  expect(isValid("([(){[]}])")).toBeTruthy();
+  expect(isValid("(([]){})")).toBeTruthy();
+  expect(isValid("(([])[{}])")).toBeTruthy();
+  expect(isValid("(([]))([{}])")).toBeTruthy();
+  expect(isValid("{(([]))}([{}])")).toBeTruthy();
+  expect(isValid("{(([]))}([}{])")).toBeFalsy();
+});
