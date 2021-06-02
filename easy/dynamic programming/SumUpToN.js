9131379cb58746475cb0809db79f2055420eed65
Monika Smulko <monika.smulko@gmail.com>
2021-06-02 12:43:00 +0200

    Reactor function by using memoization

--- easy/dynamic programming/SumUpToN.js
+++ easy/dynamic programming/SumUpToN.js
@@ -1,6 +1,15 @@
-function sumAllNumsUpToN(n) {
-  if (n < 2) return n;
-  return n + sumAllNumsUpToN(n - 1);
+function memoizeSumsUpToN() {
+  const memoizedSums = {};
+  return function RecursiveSum(n) {
+    if (n < 2) return n;
+    if (memoizedSums[n]) {
+      return memoizedSums[n];
+    }
+    memoizedSums[n] = n + RecursiveSum(n - 1);
+    return memoizedSums[n];
+  };
 }
+
+const sumAllNumsUpToN = memoizeSumsUpToN();
 
 module.exports = sumAllNumsUpToN;
