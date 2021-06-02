c1d06da07b764b30954dee5d01266f5d67a9c524
Monika Smulko <monika.smulko@gmail.com>
2021-06-02 12:19:44 +0200

    Refactor function to pass tests with recursion

--- easy/dynamic programming/SumUpToN.test.js
+++ easy/dynamic programming/SumUpToN.test.js
@@ -5,3 +5,15 @@ test("sum of n < 2 is n", () => {
   expect(sumAllNumsUpToN(1)).toBe(1);
   expect(sumAllNumsUpToN(3)).not.toBe(3);
 });
+
+test("sum of n=2 is 3", () => {
+  expect(sumAllNumsUpToN(2)).toBe(3);
+});
+
+test("sum of n=3 is 6", () => {
+  expect(sumAllNumsUpToN(3)).toBe(6);
+});
+
+test("sum of n=5 is 15", () => {
+  expect(sumAllNumsUpToN(3)).toBe(6);
+});
