9db8b8f3fbf7ff7a1a258ece5102147c65b34cec
Monika Smulko <monika.smulko@gmail.com>
2021-06-02 19:02:13 +0200

    Add recursion to a function and more tests

--- easy/dynamic programming/uniquePathsInGrid.js
+++ easy/dynamic programming/uniquePathsInGrid.js
@@ -1,6 +1,10 @@
 function findNumOfUniquePathsInGrid(height, width) {
-  const shorter = height >= width ? width : height;
-  if (shorter < 2) return shorter;
+  const shorterSide = height >= width ? width : height;
+  if (shorterSide < 2) return shorterSide;
+  return (
+    findNumOfUniquePathsInGrid(height - 1, width) +
+    findNumOfUniquePathsInGrid(height, width - 1)
+  );
 }
 
 module.exports = findNumOfUniquePathsInGrid;
