ff8b865bd8b3135c9308cfa7275349cd2eb15258
Monika Smulko <monika.smulko@gmail.com>
2021-06-03 14:52:29 +0200

    Change function to recursion to pass all tests

--- easy/dynamic programming/70.ClimbingStairs.js
+++ easy/dynamic programming/70.ClimbingStairs.js
@@ -1,20 +1,6 @@
 function climbingStairs(n) {
   if (n < 4) return n;
-  const steps = {
-    one: n,
-    two: 0,
-  };
-  let waysToClimb = 1;
-  while (steps.one > 1) {
-    steps.one -= 2;
-    steps.two++;
-    if (steps.one === 0) {
-      waysToClimb++;
-    } else {
-      waysToClimb += steps.one + steps.two;
-    }
-  }
-  return waysToClimb;
+  return climbingStairs(n - 1) + climbingStairs(n - 2);
 }
 
 module.exports = climbingStairs;
--- easy/dynamic programming/70.ClimbingStairs.test.js
+++ easy/dynamic programming/70.ClimbingStairs.test.js
@@ -13,3 +13,11 @@ test("returns 5 waysToClimb for n=4", () => {
 test("returns 8 waysToClimb for n=5", () => {
   expect(climbingStairs(5)).toBe(8);
 });
+
+test("returns 13 waysToClimb for n=6", () => {
+  expect(climbingStairs(6)).toBe(13);
+});
+
+test("returns 10946 waysToClimb for n=20", () => {
+  expect(climbingStairs(20)).toBe(10946);
+});
