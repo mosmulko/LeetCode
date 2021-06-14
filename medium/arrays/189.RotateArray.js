ad5e0097f23c0bce9653a12944fc80cf83ed5e0c
Monika Smulko <monika.smulko@gmail.com>
2021-06-14 14:11:19 +0200

    Add loop for reversing the array

--- medium/arrays/189.RotateArray.js
+++ medium/arrays/189.RotateArray.js
@@ -3,6 +3,13 @@
 
 function rotateArray(nums, k) {
   if (k === 0 || nums.length === 1) return nums;
+  for (let i = 0; i < nums.length / 2; i++) {
+    const currentNum = nums[i];
+    const indexOfOppositeNum = nums.length - 1 - i;
+    nums[i] = nums[indexOfOppositeNum];
+    nums[indexOfOppositeNum] = currentNum;
+  }
+  return nums;
 }
 
 module.exports = rotateArray;
--- medium/arrays/189.RotateArray.test.js
+++ medium/arrays/189.RotateArray.test.js
@@ -7,3 +7,7 @@ test("return unchanged array if k = 0", () => {
 test("return unchanged array if lenght of array is 1", () => {
   expect(rotateArray([1], 3)).toEqual([1]);
 });
+
+test("return rotated array if k = 1 and length is 2", () => {
+  expect(rotateArray([1, 2], 1)).toEqual([2, 1]);
+});
