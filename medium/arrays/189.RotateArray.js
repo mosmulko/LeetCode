7b283552154895accaac5e8ac2190079429ffa74
Monika Smulko <monika.smulko@gmail.com>
2021-06-14 15:43:00 +0200

    Add function that reverses parts of an array

--- medium/arrays/189.RotateArray.js
+++ medium/arrays/189.RotateArray.js
@@ -3,13 +3,30 @@
 
 function rotateArray(nums, k) {
   if (k === 0 || nums.length === 1) return nums;
-  for (let i = 0; i < nums.length / 2; i++) {
+  reversePartOfArray(nums, 0);
+  reversePartOfArray(nums, 0, k);
+  reversePartOfArray(nums, k);
+  return nums;
+}
+
+function reversePartOfArray(nums, firstToRotateIndex, loopEndIndex) {
+  let loopEnd, lastIndex;
+  if (loopEndIndex) {
+    loopEnd = loopEndIndex / 2;
+    lastIndex = loopEndIndex - 1;
+  } else {
+    loopEnd =
+      firstToRotateIndex > 0
+        ? (nums.length - firstToRotateIndex) / 2
+        : nums.length / 2;
+    lastIndex = nums.length - 1;
+  }
+  for (let i = firstToRotateIndex, j = 0; j < loopEnd; i++, j++) {
     const currentNum = nums[i];
-    const indexOfOppositeNum = nums.length - 1 - i;
+    const indexOfOppositeNum = lastIndex - j;
     nums[i] = nums[indexOfOppositeNum];
     nums[indexOfOppositeNum] = currentNum;
   }
-  return nums;
 }
 
 module.exports = rotateArray;
--- medium/arrays/189.RotateArray.test.js
+++ medium/arrays/189.RotateArray.test.js
@@ -1,13 +1,25 @@
 const rotateArray = require("./189.RotateArray");
 
-test("return unchanged array if k = 0", () => {
+test("returns unchanged array if k = 0", () => {
   expect(rotateArray([1, 2, 3, 4], 0)).toEqual([1, 2, 3, 4]);
 });
 
-test("return unchanged array if lenght of array is 1", () => {
+test("returns unchanged array if lenght of array is 1", () => {
   expect(rotateArray([1], 3)).toEqual([1]);
 });
 
-test("return rotated array if k = 1 and length is 2", () => {
+test("returns rotated array if k = 1 and length is 2", () => {
   expect(rotateArray([1, 2], 1)).toEqual([2, 1]);
+});
+
+test("returns rotated array if k=1 and length > k*2", () => {
+  expect(rotateArray([1, 2, 3], 1)).toEqual([3, 1, 2]);
+  expect(rotateArray([1, 2, 3, 4], 1)).toEqual([4, 1, 2, 3]);
+  expect(rotateArray([1, 2, 3, 4, 5, 6], 1)).toEqual([6, 1, 2, 3, 4, 5]);
+});
+
+test("returns rotated array if k > 1 and length > k*2", () => {
+  // expect(rotateArray([1, 2, 3], 2)).toEqual([2, 3, 1]);
+  // expect(rotateArray([1, 2, 3, 4], 3)).toEqual([2, 3, 4, 1]);
+  expect(rotateArray([1, 2, 3, 4, 5, 6], 2)).toEqual([5, 6, 1, 2, 3, 4]);
 });
