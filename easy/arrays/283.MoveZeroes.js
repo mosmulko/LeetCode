20d0b5a184df758ac65fc2930cbd88b980de12ae
Monika Smulko <monika.smulko@gmail.com>
2021-06-15 13:19:02 +0200

    Add final changes

--- easy/arrays/283.MoveZeroes.js
+++ easy/arrays/283.MoveZeroes.js
@@ -1,14 +1,15 @@
 function moveZeroes(nums) {
-  let nextIndexToFill = 0;
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] !== 0) {
-      nums[++nextIndexToFill] = nums[i];
-    }
-    if (i !== nextIndexToFill) {
+  let indexToFill;
+  for (let i = 0; i < nums.length; i++) {
+    if (indexToFill !== undefined && nums[i] !== 0) {
+      nums[indexToFill] = nums[i];
       nums[i] = 0;
+      indexToFill++;
+    }
+    if (indexToFill === undefined && nums[i] === 0) {
+      indexToFill = i;
     }
   }
-  console.log(nums);
   return nums;
 }
 
--- easy/arrays/283.MoveZeroes.test.js
+++ easy/arrays/283.MoveZeroes.test.js
@@ -6,9 +6,19 @@ test("returns unchanged array if there are no zeroes or 0 is at the end", () => 
   expect(moveZeroes([1, 2, 7, 9, 10, 0])).toEqual([1, 2, 7, 9, 10, 0]);
 });
 
-test("moves 0 to the end if there are zeros, but non at the beginning", () => {
+test("returns changed array if there are zeros, but non at the beginning", () => {
   expect(moveZeroes([1, 0, 2, 0, 3, 0, 4])).toEqual([1, 2, 3, 4, 0, 0, 0]);
   expect(moveZeroes([1, 2, 7, 0, 0, 9, 0, 10])).toEqual([
     1, 2, 7, 9, 10, 0, 0, 0,
+  ]);
+});
+
+test("returns changed array if there is zero at the beginning", () => {
+  expect(moveZeroes([0, 1, 2, 3, 4])).toEqual([1, 2, 3, 4, 0]);
+  expect(moveZeroes([0, 0, 1, 2, 0, 0, 7, 9, 10])).toEqual([
+    1, 2, 7, 9, 10, 0, 0, 0, 0,
+  ]);
+  expect(moveZeroes([0, 0, 0, 1, 0, 2, 7, 9, 0, 0, 10, 0])).toEqual([
+    1, 2, 7, 9, 10, 0, 0, 0, 0, 0, 0, 0,
   ]);
 });
