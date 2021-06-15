f16b6c0a219e8af74dd1ede7944a0863f42c6ca6
Monika Smulko <monika.smulko@gmail.com>
2021-06-15 22:02:29 +0200

    Refactor code to pass all tests

--- easy/arrays/350.Intersection.js
+++ easy/arrays/350.Intersection.js
@@ -1,5 +1,19 @@
 function findIntersection(nums1, nums2) {
   if (nums1.length === 0 || nums2.length === 0) return [];
+  const contentOfNums1 = {};
+  for (let i = 0; i < nums1.length; i++) {
+    contentOfNums1[nums1[i]]
+      ? contentOfNums1[nums1[i]]++
+      : (contentOfNums1[nums1[i]] = 1);
+  }
+  const intersection = [];
+  for (let i = 0; i < nums2.length; i++) {
+    if (contentOfNums1[nums2[i]]) {
+      intersection.push(nums2[i]);
+      contentOfNums1[nums2[i]]--;
+    }
+  }
+  return intersection;
 }
 
 module.exports = findIntersection;
--- easy/arrays/350.Intersection.test.js
+++ easy/arrays/350.Intersection.test.js
@@ -4,3 +4,13 @@ test("returns empty array if one of arguments is empty", () => {
   expect(findIntersection([], [1, 2, 3])).toEqual([]);
   expect(findIntersection([1, 2], [])).toEqual([]);
 });
+
+test("returns array of 1 intersection", () => {
+  expect(findIntersection([2], [1, 2, 3])).toContain(2);
+  expect(findIntersection([3], [1, 2, 3])).toContain(3);
+});
+
+test("returns array of all intersection numbers", () => {
+  expect(findIntersection([4, 9, 5], [9, 4, 9, 8, 4])).toContain(4);
+  expect(findIntersection([4, 9, 5], [9, 4, 9, 8, 4])).toContain(9);
+});
