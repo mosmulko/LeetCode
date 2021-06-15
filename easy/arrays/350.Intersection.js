6da90daa530eb8a34d3f2f1fac989d44b273ed0f
Monika Smulko <monika.smulko@gmail.com>
2021-06-15 22:10:05 +0200

    Change for loops to forEach and var names

--- easy/arrays/350.Intersection.js
+++ easy/arrays/350.Intersection.js
@@ -1,18 +1,16 @@
-function findIntersection(nums1, nums2) {
-  if (nums1.length === 0 || nums2.length === 0) return [];
-  const contentOfNums1 = {};
-  for (let i = 0; i < nums1.length; i++) {
-    contentOfNums1[nums1[i]]
-      ? contentOfNums1[nums1[i]]++
-      : (contentOfNums1[nums1[i]] = 1);
-  }
+function findIntersection(first, second) {
+  if (first.length === 0 || second.length === 0) return [];
+  const numsFromFirst = {};
+  first.forEach((num) => {
+    numsFromFirst[num] ? numsFromFirst[num]++ : (numsFromFirst[num] = 1);
+  });
   const intersection = [];
-  for (let i = 0; i < nums2.length; i++) {
-    if (contentOfNums1[nums2[i]]) {
-      intersection.push(nums2[i]);
-      contentOfNums1[nums2[i]]--;
+  second.forEach((num) => {
+    if (numsFromFirst[num]) {
+      intersection.push(num);
+      numsFromFirst[num]--;
     }
-  }
+  });
   return intersection;
 }
 
