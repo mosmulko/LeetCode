304360dda4ada55d04bedbff2630305be9d34e83
Monika Smulko <monika.smulko@gmail.com>
2021-06-16 12:55:18 +0200

    Add validation for columns

--- medium/arrays/36.ValidSudoku.js
+++ medium/arrays/36.ValidSudoku.js
@@ -1,12 +1,17 @@
 function isSudokuValid(board) {
   const rows = {};
+  const columns = {};
+  for (let i = 0; i < 9; i++) {
+    rows[i] = {};
+    columns[i] = {};
+  }
   for (let row = 0; row < board.length; row++) {
-    rows[row] = {};
     for (let col = 0; col < board.length; col++) {
       const field = board[row][col];
       if (field !== ".") {
-        if (rows[row][field]) return false;
+        if (rows[row][field] || columns[col][field]) return false;
         rows[row][field] = true;
+        columns[col][field] = true;
       }
     }
   }
