dc45b14dad208ec71d1a89ace87f40ba1a06d51e
Monika Smulko <monika.smulko@gmail.com>
2021-06-16 23:17:52 +0200

    Add check for 3x3 boxes

--- medium/arrays/36.ValidSudoku.js
+++ medium/arrays/36.ValidSudoku.js
@@ -1,17 +1,22 @@
 function isSudokuValid(board) {
   const rows = {};
   const columns = {};
+  const boxes = {};
   for (let i = 0; i < 9; i++) {
     rows[i] = {};
     columns[i] = {};
+    boxes[i] = {};
   }
   for (let row = 0; row < board.length; row++) {
-    for (let col = 0; col < board.length; col++) {
+    for (let col = 0; col < board[row].length; col++) {
       const field = board[row][col];
+      const box = Math.floor(row / 3) * 3 + Math.floor(col / 3);
       if (field !== ".") {
-        if (rows[row][field] || columns[col][field]) return false;
+        if (rows[row][field] || columns[col][field] || boxes[box][field])
+          return false;
         rows[row][field] = true;
         columns[col][field] = true;
+        boxes[box][field] = true;
       }
     }
   }
