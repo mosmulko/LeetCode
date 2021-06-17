7fe6c8c4a3e8bc54472de5f883d1371ca00fe8b6
Monika Smulko <monika.smulko@gmail.com>
2021-06-17 11:47:02 +0200

    Add code to rotate matrix 2x2

--- medium/arrays/48.RotateImage.js
+++ medium/arrays/48.RotateImage.js
@@ -1,5 +1,23 @@
 function rotateMatrix(matrix) {
   if (matrix.length <= 1) return matrix;
+
+  for (let nestedArray = 0; nestedArray < matrix.length; nestedArray++) {
+    reverseArray(matrix[nestedArray]);
+  }
+  const currentNum = matrix[0][0];
+  const numToSwap = matrix[1][1];
+  matrix[0][0] = numToSwap;
+  matrix[1][1] = currentNum;
+  return matrix;
+}
+
+function reverseArray(array) {
+  for (let i = 0; i < array.length / 2; i++) {
+    const currentElement = array[i];
+    const indexOfOppositeElement = array.length - 1 - i;
+    array[i] = array[indexOfOppositeElement];
+    array[indexOfOppositeElement] = currentElement;
+  }
 }
 
 module.exports = rotateMatrix;
--- medium/arrays/48.RotateImage.test.js
+++ medium/arrays/48.RotateImage.test.js
@@ -1,6 +1,25 @@
 const rotateMatrix = require("./48.RotateImage");
 
+const twoByTwoMatrix = [
+  [1, 2],
+  [3, 4],
+];
+const twoByTwoRotatedMatrix = [
+  [3, 1],
+  [4, 2],
+];
+
+// const threeByThreeMatrix = [[1,2,3],[4,5,6],[7,8,9]];
+// const threeByRotatedThreeMatrix = [[7,4,1],[8,5,2],[9,6,3]];
+
+// const fourByfourMatrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]];
+// const fourByfourRotatedMatrix = [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]];
+
 test("returns unchanged array if no matrix is passed as argument", () => {
   expect(rotateMatrix([])).toEqual([]);
   expect(rotateMatrix([1])).toEqual([1]);
+});
+
+test("returns rotated matrix", () => {
+  expect(rotateMatrix(twoByTwoMatrix)).toEqual(twoByTwoRotatedMatrix);
 });
