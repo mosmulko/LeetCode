ee47de8ccd82b22b952dbf91c7020c6256fbaaf5
Monika Smulko <monika.smulko@gmail.com>
2021-06-19 19:31:51 +0200

    Add code for checking all valid left axises

--- medium/arrays/ConnectFour.js
+++ medium/arrays/ConnectFour.js
@@ -3,6 +3,7 @@ function whoWonConnectFour(board) {
   const rowLength = board[0].length;
   const rows = {};
   const columns = {};
+  const leftAxises = {};
 
   for (let i = 0; i < length; i++) {
     rows[i] = [];
@@ -12,12 +13,21 @@ function whoWonConnectFour(board) {
     columns[i] = [];
   }
 
+  for (let i = 4 - rowLength; i <= length - 4; i++) {
+    leftAxises[i] = [];
+  }
+
   for (let row = 0; row < length; row++) {
     for (let col = 0; col < rowLength; col++) {
       const field = board[row][col];
       if (field) {
         addOrReplacePawn(rows[row], field);
         addOrReplacePawn(columns[col], field);
+        if (leftAxises[row - col]) {
+          addOrReplacePawn(leftAxises[row - col], field);
+          if (leftAxises[row - col].length === 4) return field;
+        }
+
         if (rows[row].length === 4 || columns[col].length === 4) return field;
       }
     }
--- medium/arrays/__tests__/ConnectFour.test.js
+++ medium/arrays/__tests__/ConnectFour.test.js
@@ -24,15 +24,27 @@ const boardOInColumn = [
   ["O", "X", "X", "X", "O", null, null],
   ["O", "X", "O", "O", "O", "X", null],
 ];
+const boardXInLeftAxis = [
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null],
+  [null, "X", null, null, "X", null, null],
+  ["X", "O", "X", null, "O", null, null],
+  ["O", "X", "X", "X", "O", null, null],
+  ["O", "X", "O", "O", "X", "X", null],
+];
 
 test("returns null if game hasn't started yet or there's draw", () => {
   expect(whoWonConnectFour(emptyBoard)).toBe(null);
 });
 
-test("returns X if X has 4 in a row", () => {
+test("returns X when X has 4 in a row", () => {
   expect(whoWonConnectFour(boardXInRow)).toBe("X");
 });
 
-test("returns O if O has 4 in a column", () => {
+test("returns O when O has 4 in a column", () => {
   expect(whoWonConnectFour(boardOInColumn)).toBe("O");
+});
+
+test("returns X when one of left axis has 4 Xs", () => {
+  expect(whoWonConnectFour(boardXInLeftAxis)).toBe("X");
 });
