909e739d1466df94f41534746a95c58a3ec17241
Monika Smulko <monika.smulko@gmail.com>
2021-06-21 13:09:09 +0200

    Fix bug for when null is between 4 pawns

--- medium/arrays/ConnectFour.js
+++ medium/arrays/ConnectFour.js
@@ -4,8 +4,8 @@ function whoWonConnectFour(board) {
   const rowLength = board[0].length;
   const rows = {};
   const columns = {};
-  const leftAxises = {};
-  const rightAxises = {};
+  const leftAxes = {};
+  const rightAxes = {};
 
   for (let i = 0; i < length; i++) {
     rows[i] = [];
@@ -16,32 +16,30 @@ function whoWonConnectFour(board) {
   }
 
   for (let i = -(rowLength - numOfFields); i <= length - numOfFields; i++) {
-    leftAxises[i] = [];
+    leftAxes[i] = [];
   }
 
   for (let i = numOfFields - 1; i < rowLength - numOfFields + length; i++) {
-    rightAxises[i] = [];
+    rightAxes[i] = [];
   }
 
   for (let row = 0; row < length; row++) {
     for (let col = 0; col < rowLength; col++) {
       const field = board[row][col];
-      if (field) {
-        addOrReplacePawn(rows[row], field);
-        addOrReplacePawn(columns[col], field);
-        if (
-          rows[row].length === numOfFields ||
-          columns[col].length === numOfFields
-        )
-          return field;
-        if (leftAxises[row - col]) {
-          addOrReplacePawn(leftAxises[row - col], field);
-          if (leftAxises[row - col].length === numOfFields) return field;
-        }
-        if (rightAxises[row + col]) {
-          addOrReplacePawn(rightAxises[row + col], field);
-          if (rightAxises[row + col].length === numOfFields) return field;
-        }
+      addOrReplacePawn(rows[row], field);
+      addOrReplacePawn(columns[col], field);
+      if (
+        rows[row].length === numOfFields ||
+        columns[col].length === numOfFields
+      )
+        return field;
+      if (leftAxes[row - col]) {
+        addOrReplacePawn(leftAxes[row - col], field);
+        if (leftAxes[row - col].length === numOfFields) return field;
+      }
+      if (rightAxes[row + col]) {
+        addOrReplacePawn(rightAxes[row + col], field);
+        if (rightAxes[row + col].length === numOfFields) return field;
       }
     }
   }
@@ -49,10 +47,15 @@ function whoWonConnectFour(board) {
 }
 
 function addOrReplacePawn(arr, pawn) {
+  if (arr.length === 0 && !pawn) return;
   if (arr[arr.length - 1] === pawn) {
     arr.push(pawn);
   } else {
-    arr.splice(0, arr.length, pawn);
+    if (pawn) {
+      arr.splice(0, arr.length, pawn);
+    } else {
+      arr.splice(0, arr.length);
+    }
   }
 }
 
--- medium/arrays/__tests__/ConnectFour.test.js
+++ medium/arrays/__tests__/ConnectFour.test.js
@@ -8,6 +8,14 @@ const emptyBoard = [
   [null, null, null, null, null, null, null],
   [null, null, null, null, null, null, null],
 ];
+const boardNoWin = [
+  [null, null, null, null, null, null, null],
+  [null, null, null, null, null, "X", null],
+  [null, null, "X", null, null, "X", null],
+  [null, null, null, null, null, null, null],
+  [null, "X", null, "X", "X", "X", null],
+  ["O", "X", "O", "O", "O", "X", null],
+];
 const boardXInRow = [
   [null, null, null, null, null, null, null],
   [null, null, null, null, null, null, null],
@@ -41,8 +49,9 @@ const boardOInRightAxis = [
   ["O", "X", "O", "O", "X", "X", null],
 ];
 
-test("returns null if game hasn't started yet or there's draw", () => {
+test("returns null if nobody won yet", () => {
   expect(whoWonConnectFour(emptyBoard)).toBe(null);
+  expect(whoWonConnectFour(boardNoWin)).toBe(null);
 });
 
 test("returns X when X has 4 in a row", () => {
