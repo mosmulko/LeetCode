36cb0ba5b48591aba9589e963e3bf527cfcd1eec
Monika Smulko <monika.smulko@gmail.com>
2021-06-28 23:04:45 +0200

    Add code for when only unique letters

--- medium/strings/3.LongestSubstring.js
+++ medium/strings/3.LongestSubstring.js
@@ -2,6 +2,17 @@
 
 function findLongestSubstring(input) {
   if (!input) return 0;
+  if (input.length === 1) return 1;
+  const uniqueLetters = {};
+  let substringLength = 0;
+  for (let i = 0; i < input.length; i++) {
+    const letter = input[i];
+    if (!uniqueLetters[letter]) {
+      substringLength++;
+      uniqueLetters[letter] = true;
+    }
+  }
+  return substringLength;
 }
 
 module.exports = findLongestSubstring;
--- medium/strings/__tests__/3.LongestSubstring.test.js
+++ medium/strings/__tests__/3.LongestSubstring.test.js
@@ -3,3 +3,14 @@ const findLongestSubstring = require("../3.LongestSubstring");
 test("return 0 if no input", () => {
   expect(findLongestSubstring("")).toBe(0);
 });
+
+test("return 1 if input has length 1", () => {
+  expect(findLongestSubstring("p")).toBe(1);
+});
+
+test("return string's length when it has all unique characters", () => {
+  const uniqueLetters = "parody";
+  const repeatedLetter = "parrot";
+  expect(findLongestSubstring(uniqueLetters)).toBe(uniqueLetters.length);
+  expect(findLongestSubstring(repeatedLetter)).not.toBe(repeatedLetter.length);
+});
