13cc587598d1546540f12e7ebb8f46642dd153fc
Monika Smulko <monika.smulko@gmail.com>
2021-06-29 22:31:25 +0200

    WIP

--- medium/strings/5.PalindromeSubstring.js
+++ medium/strings/5.PalindromeSubstring.js
@@ -1,15 +1,38 @@
 function findLongestPalindromeSubstring(input) {
-  if (!input) return 0;
-  if (input.length === 1) return 1;
-  const uniqueLetters = new Set();
-  let substringLength = 0;
+  if (input.length < 2) return input;
+  const letters = {};
+  const candidates = [];
   for (let i = 0; i < input.length; i++) {
     const letter = input[i];
-    if (!uniqueLetters.has(letter)) {
-      uniqueLetters.add(letter);
+    if (!letters[letter]) {
+      letters[letter] = [];
+    } else {
+      letters[letter].forEach((prevIndex) => candidates.push([prevIndex, i]));
     }
+    letters[letter].push(i);
   }
-  return uniqueLetters.size === input.length ? 1 : substringLength;
+  candidates.sort(compareLength);
+  candidates.forEach((arr) => {
+    let first = arr[0] + 1;
+    let reflection = arr[1] - 1;
+    const length = (reflection - first + 1) / 2;
+    let isPalindrome = true;
+    for (let i = first; i <= length; i++, reflection--) {
+      isPalindrome = input[i] === input[reflection];
+    }
+    if (isPalindrome) console.log(input.substring(arr[0], arr[1] + 1));
+  });
+  return input[0];
+}
+
+function compareLength(a, b) {
+  if (a[1] - a[0] > b[1] - b[0]) {
+    return -1;
+  }
+  if (a[1] - a[0] < b[1] - b[0]) {
+    return 1;
+  }
+  return 0;
 }
 
 module.exports = findLongestPalindromeSubstring;
--- medium/strings/__tests__/5.PalindromeSubstring.test.js
+++ medium/strings/__tests__/5.PalindromeSubstring.test.js
@@ -1,10 +1,16 @@
 const findLongestPalindromeSubstring = require("../5.PalindromeSubstring");
 
 test("return 0 if no input", () => {
-  expect(findLongestPalindromeSubstring("")).toBe(0);
+  expect(findLongestPalindromeSubstring("")).toBe("");
 });
 
 test("return 1 if input has length 1 or only unique characters", () => {
-  expect(findLongestPalindromeSubstring("p")).toBe(1);
-  expect(findLongestPalindromeSubstring("bac")).toBe(1);
+  expect(findLongestPalindromeSubstring("p")).toBe("p");
+  expect(findLongestPalindromeSubstring("bac")).toBe("b");
+});
+
+test("return length of longest palindrome substring", () => {
+  expect(findLongestPalindromeSubstring("abcabcbb")).toBe("bcb");
+  expect(findLongestPalindromeSubstring("pwwkekw")).toBe("wkekw");
+  expect(findLongestPalindromeSubstring("babcgdedgb")).toBe("gdedg");
 });
