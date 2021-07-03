13938480d9fcbf22e1f741ba90a7548e73a67315
Monika Smulko <monika.smulko@gmail.com>
2021-07-03 19:36:04 +0200

    Clean up code

--- medium/strings/5.PalindromeSubstring.js
+++ medium/strings/5.PalindromeSubstring.js
@@ -2,42 +2,38 @@ function findLongestPalindromeSubstring(input) {
   if (input.length < 2) return input;
   let longestPalindrome = input[0];
   for (let i = 0; i < input.length; ) {
-    let index = i;
-    let mirroredIndex = i + 1;
+    let current = i;
+    let next = i + 1;
     let palindrome = "";
 
-    if (input[i] === input[i + 1] && input[i] === input[i + 2]) {
-      palindrome += input[i] + input[i] + input[i];
-      mirroredIndex = i + 3;
-      while (input[index] === input[mirroredIndex]) {
-        palindrome += input[index];
-        mirroredIndex++;
-      }
-      index = i - 1;
-    }
-
-    if (input[i] === input[i + 2] && input[i] !== input[i + 1]) {
+    if (input[i] === input[i + 2]) {
       palindrome += input[i] + input[i + 1] + input[i];
-      index = i - 1;
-      mirroredIndex = i + 3;
+      next = i + 3;
+      if (input[i] === input[i + 1]) {
+        while (input[current] === input[next]) {
+          palindrome += input[current];
+          next++;
+        }
+      }
+      current = i - 1;
     }
 
     while (
-      input[index] === input[mirroredIndex] &&
-      index >= 0 &&
-      mirroredIndex < input.length
+      input[current] === input[next] &&
+      current >= 0 &&
+      next < input.length
     ) {
-      const letter = input[index];
+      const letter = input[current];
       palindrome = letter + palindrome + letter;
-      index--;
-      mirroredIndex++;
+      current--;
+      next++;
     }
 
     if (palindrome.length > longestPalindrome.length) {
       longestPalindrome = palindrome;
     }
 
-    i = mirroredIndex;
+    i = next;
   }
   return longestPalindrome;
 }
