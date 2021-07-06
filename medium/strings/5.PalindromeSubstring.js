5c6658c88a8d05cd433d667ba46f69f1cd73dfe4
Monika Smulko <monika.smulko@gmail.com>
2021-07-06 23:58:07 +0200

    Expand around center WIP

--- medium/strings/5.PalindromeSubstring.js
+++ medium/strings/5.PalindromeSubstring.js
@@ -1,58 +1,27 @@
 function findLongestPalindromeSubstring(input) {
   if (input.length < 2) return input;
-  let isPalindrome = true;
-  for (let i = 0; i < input.length / 2; i++) {
-    const reflection = input.length - 1 - i;
-    if (input[i] !== input[reflection]) {
-      isPalindrome = false;
+  let startIndexOfMaxPalindrome = 0;
+  let lengthOfMaxPalindrome = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    const lengthOfPalAtChar = expandAroundCenter(i, i);
+    const lengthOfPalAfterChar = expandAroundCenter(i, i + 1);
+    const maxLengthAtIndex = Math.max(lengthOfPalAtChar, lengthOfPalAfterChar);
+    if (maxLengthAtIndex > lengthOfMaxPalindrome) {
+      lengthOfMaxPalindrome = maxLengthAtIndex;
+      startIndexOfMaxPalindrome = i - Math.floor((maxLengthAtIndex - 1) / 2);
     }
   }
-  if (isPalindrome) return input;
-  let longestPalindrome = input[0];
-  let lengthOfMaxCandidate = input.length;
-  let index = 0;
-  while (
-    index < input.length &&
-    longestPalindrome.length < lengthOfMaxCandidate
-  ) {
-    let first = index;
-    let second = index + 1;
-    let third = index + 2;
-    let palindrome = "";
 
-    if (input[first] === input[third]) {
-      const letter = input[first];
-      palindrome += letter + input[second] + letter;
-      second = third + 1;
-      first -= 1;
-    } else if (
-      input[first] === input[second] &&
-      input[first] !== input[third]
-    ) {
-      const letter = input[first];
-      palindrome += letter + letter;
-      second = third;
-      first -= 1;
+  function expandAroundCenter(left, right) {
+    while (left >= 0 && right < input.length && input[left] === input[right]) {
+      left--;
+      right++;
     }
-
-    while (
-      input[first] === input[second] &&
-      first >= 0 &&
-      second < input.length
-    ) {
-      const letter = input[first];
-      palindrome = letter + palindrome + letter;
-      first--;
-      second++;
-    }
-
-    if (palindrome.length > longestPalindrome.length) {
-      longestPalindrome = palindrome;
-    }
-    lengthOfMaxCandidate = (input.length - index) * 2 - 1;
-    index++;
+    return right - left - 1;
   }
-  return longestPalindrome;
+
+  return input.substring(startIndexOfMaxPalindrome, lengthOfMaxPalindrome);
 }
 
 module.exports = findLongestPalindromeSubstring;
