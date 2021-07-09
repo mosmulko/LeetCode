c37c562e9b782cdb9b7b7770e8c0b1bd0d2fa021
Monika Smulko <monika.smulko@gmail.com>
2021-07-09 13:25:33 +0200

    Refactor code so the element is deleted

--- easy/arrays/27.RemoveElement.js
+++ easy/arrays/27.RemoveElement.js
@@ -2,27 +2,17 @@ function removeElement(nums, value) {
   let k = 0;
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== value) {
-      nums[k] = nums[i];
+      if (k !== i) {
+        nums[k] = nums[i];
+        nums[i] = null;
+      }
       k++;
+    } else {
+      nums[i] = null;
     }
   }
   console.log(nums);
   return k;
 }
-
-// function removeElement(nums, value) {
-//   let indexToFill = 0;
-//   for (let i = 0; i < nums.length; i++) {
-//     if (nums[i] !== value) {
-//       nums[indexToFill] = nums[i];
-//       indexToFill++;
-//     }
-//     if (indexToFill > 0) {
-//       nums[i] = null;
-//     }
-//   }
-//   console.log(nums);
-//   return indexToFill;
-// }
 
 module.exports = removeElement;
