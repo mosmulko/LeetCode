4aa7085329a5bb3ebdebf98c93ecf653d08a5c63
Monika Smulko <monika.smulko@gmail.com>
2021-07-09 11:46:28 +0200

    Clean up function

--- easy/arrays/27.RemoveElement.js
+++ easy/arrays/27.RemoveElement.js
@@ -1,22 +1,19 @@
 function removeElement(nums, value) {
   let indexToFill;
-  let remainingLength = nums.length;
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === value) {
-      remainingLength--;
-    }
     if (indexToFill === undefined && nums[i] === value) {
       indexToFill = i;
-      nums[i] = null;
-    } else if (indexToFill !== undefined) {
+    }
+    if (indexToFill !== undefined) {
       if (nums[i] !== value) {
         nums[indexToFill] = nums[i];
         indexToFill++;
       }
       nums[i] = null;
+      if (i === nums.length - 1) return indexToFill;
     }
   }
-  return remainingLength;
+  return nums.length;
 }
 
 module.exports = removeElement;
